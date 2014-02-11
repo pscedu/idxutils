4ae52d1163145b63be7377249fc31da42d8a7722
beckert <beckert@3eda493b-6a19-0410-b2e0-ec8ea4dd8fda>
2014-02-11 00:32:35 +0000

    added support for multiple files
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/apps/idxsearch@23139 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- idxsearch.py
+++ idxsearch.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python2
 
-import argparse
-import os
-import struct
-import sys
+import argparse, os, struct, sys
+
 
 # usage: idxsearch.py -k 8 869a mkidx.out.00
 parser = argparse.ArgumentParser()
@@ -11,7 +9,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-k", "--key-size", type=int, default = 8, help="length of key")
 parser.add_argument("-v", "--val-size", type=int, default = 8, help="length of value")
 parser.add_argument("key")
-parser.add_argument("file")
+parser.add_argument("files", nargs="+")
 
 args = parser.parse_args()
 
@@ -23,29 +21,29 @@ def read_chunk(fd):
     chunk = os.read(fd, chunk_size)
     return struct.unpack("8sQ", chunk)
 
-file_size = os.stat(args.file).st_size
-chunks = file_size / chunk_size
-chunk = chunks / 2
-
-fd = os.open(args.file, os.O_RDONLY)
-os.lseek(fd, chunk*chunk_size, os.SEEK_SET)
-
-max = chunks
-min = 0
-while 0 <= chunk and chunk < chunks:
-    (key, value) = read_chunk(fd)
-    if args.key > key:
-        min = chunk
-        chunk = (chunk + max) / 2	
-    elif args.key < key:
-        max = chunk
-        chunk = chunk / 2 
-    else:
-        print value
-        os.close(fd)
-        sys.exit(0)
-    os.lseek(fd, chunk * chunk_size, os.SEEK_SET)
-        
-os.close(fd)
-print "key not found."
-sys.exit(1)
+for f in args.files:
+    file_size = os.stat(f).st_size
+    chunks = file_size / chunk_size
+    chunk = chunks / 2
+    
+    fd = os.open(f, os.O_RDONLY)
+    os.lseek(fd, chunk*chunk_size, os.SEEK_SET)
+    
+    max = chunks
+    min = 0
+    while 0 <= chunk and chunk < chunks:
+        (key, value) = read_chunk(fd)
+        if args.key > key:
+            min = chunk
+            chunk = (chunk + max) / 2	
+        elif args.key < key:
+            max = chunk
+            chunk = chunk / 2 
+        else:
+            print value
+            os.close(fd)
+            sys.exit(0)
+        os.lseek(fd, chunk * chunk_size, os.SEEK_SET)
+    os.close(fd)
+    print "key not found."
+    sys.exit(1)
