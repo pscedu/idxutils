e9bf3c8c613ab015aac900d45f0a1d44e106d2e0
ganas <ganas@3eda493b-6a19-0410-b2e0-ec8ea4dd8fda>
2014-02-24 05:05:49 +0000

    Fixed boundary bug and struct alignment.
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/apps/idxsearch@23230 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- idxsearch.py
+++ idxsearch.py
@@ -7,10 +7,16 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("-k", "--key-size", type=int, default = 8, help="length of key")
 parser.add_argument("-v", "--val-size", type=int, default = 8, help="length of value")
+parser.add_argument("-d", "--debug", default=False, nargs="?", help="debug output")
 parser.add_argument("key")
-parser.add_argument("files", default = [], nargs="+")
+parser.add_argument("files", nargs="+")
 
 args = parser.parse_args()
+
+struct_format = "=" + str(args.key_size) + "sQ"
+
+if args.key.startswith("0x"):
+    args.key = args.key[2:]
 
 args.key = " "*(args.key_size - len(args.key)) + args.key
 
@@ -18,30 +24,44 @@ chunk_size = args.key_size + args.val_size
 
 def read_chunk(fd):
     chunk = os.read(fd, chunk_size)
-    return struct.unpack("8sQ", chunk)
+    return struct.unpack(struct_format, chunk)
+
+found = False
 
 for f in args.files:
     file_size = os.stat(f).st_size
-    chunks = file_size / chunk_size
+    chunks = file_size / chunk_size - 1
     chunk = chunks / 2
-    
+
+    if args.debug:
+        print "[Opened %s with %s chunks]" % (f, chunks)
+
     fd = os.open(f, os.O_RDONLY)
-    os.lseek(fd, chunk*chunk_size, os.SEEK_SET)
-    
-    max = chunks
-    min = 0
-    while 0 <= chunk and chunk < chunks-1:
+
+    chunk_max = chunks
+    chunk_min = 0
+    while chunk_max >= chunk_min:
+
+        mid = chunk_min + ((chunk_max - chunk_min)/2)
+
+        if args.debug:
+            print "New bounds [%s, %s, %s]" % (chunk_min, mid, chunk_max)
+
+        os.lseek(fd, (mid) * chunk_size, os.SEEK_SET)
         (key, value) = read_chunk(fd)
-        if args.key > key:
-            min = chunk
-            chunk = (chunk + max) / 2	
-        elif args.key < key:
-            max = chunk
-            chunk = chunk / 2 
+
+        if key == args.key:
+            print "found", key
+            found = True
+            break
+        elif key < args.key:
+            chunk_min = mid + 1
         else:
-            print value
-            os.close(fd)
-            sys.exit(0)
-        os.lseek(fd, chunk * chunk_size, os.SEEK_SET)
+            chunk_max = mid - 1
     os.close(fd)
-print "key not found."
+
+    if found:
+        sys.exit(1)
+
+if not found:
+    print "not found"
